8be0abdffb07e7a05b5b9b932e94c8b9d61d98b7
Tayvon Lewis <tayvonlewis0@gmail.com>
2020-11-11 16:33:43 -0600

    validate user data

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
+
+const { validateRegisterInput } = require('../../util/validators')
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
@@ -13,8 +15,12 @@ module.exports = {
         registerInput: { username, email, password, confirmPassword }
       },
     ) {
-      //TODO: Validate user data
-      //TODO: Make sure user doesnt already exist
+      //Validate user data
+      const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+      //Make sure user doesnt already exist
       const user = await User.findOne({ username });
       if (user) {
         throw new UserInputError('Username is taken', {
